cde74a9b20f9102385dc77daca9dea2b8221d059
echelon <slvt16@vt.edu>
2020-09-25 12:10:04 -0400

    Update setup.py
    +dev/test req

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup, find_packages
 
+dev_req = [
+    "black>=19.10b0",
+    "pylint>=2.5.3",
+]
+
+test_req = [
+    "pytest>=5.4.3",
+]
+
+
 
 setup(
     name="project",
@@ -10,6 +20,7 @@ setup(
     url="<project or repo url>",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=["black", "python-dotenv", "pylint", "pytest",],
+    install_requires=[], # app dependencies
+    extras_require={"dev": test_req + dev_req, "test": test_req,}, 
 )
 
